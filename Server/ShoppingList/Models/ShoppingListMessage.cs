8137fe8c5edccf1becfb1271a3c0b3a81e948102
ayobnas@gmail.com <ayobnas@gmail.com>
2022-02-14 18:48:07 +0200

    models

---
+++ Server/ShoppingList/Models/ShoppingListMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingListMessage
+    {
+        public int ListID { get; set; }
+        public int UserID { get; set; }
+        public string Message { get; set; }
+        public string CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Server/ShoppingList/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+       
+        public string Img { get; set; }
+        public bool IsActive { get; set; }
+
+        public string NotificationToken { get; set; }
+    }
+}
