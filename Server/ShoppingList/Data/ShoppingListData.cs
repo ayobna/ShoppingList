bcbdcdfbe33fadc1e2ebb81ab7d5e434aa6c597f
ayobnas@gmail.com <ayobnas@gmail.com>
2022-02-15 16:20:59 +0200

    add swagger test

---
+++ Server/ShoppingList/Data/ShoppingListData.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Data
+{
+    public class ShoppingListData
+    {
+
+        private readonly DbConnection db = new DbConnection();
+
+        //proc not Exsits yet
+        public List<Shoppinglist> GetShoppingList()
+        {
+            SqlCommand cmd = db.CreateCommand("Proc_Get_District", db.Connect(), "proc");
+            DataTable tb = db.ReadAndClose(cmd);
+            List<Shoppinglist> shoppinglist = db.ConvertDataTable<Shoppinglist>(tb);
+            return shoppinglist;
+        }
+    }
+}
