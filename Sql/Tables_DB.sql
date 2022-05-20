9fe1bc7dedf6bd240d51adf354f33ef5f1ff6343
ayobna <ayobnas@gmail.com>
2022-05-20 10:17:28 +0300

    sql update proc

--- Sql/Tables_DB.sql
+++ Sql/Tables_DB.sql
@@ -143,60 +143,55 @@ go
 
 -------------------------------------------------User----------------------------------------------
 
-Create Proc Proc_Get_Users
-AS
-Select * From [users]
-Go
+--Create Proc Proc_Get_Users
+--AS
+--Select * From [users]
+--Go
 
---exec Proc_Get_Users
+----exec Proc_Get_Users
 
-create Proc Proc_Create_User
-@Email nvarchar (150),
-@FirstName nvarchar (150),
-@LastName nvarchar (150),
-@Password nvarchar(max),
-@PhoneNumber nvarchar (150),
-@Img nvarchar(max),
-@IsActive bit,
-@NotificationToken nvarchar(max),
-@UserID int output
-AS
-IF NOT EXISTS (Select * From [users] Where Email=@Email or PhoneNumber=@PhoneNumber) 
-Begin
-    Insert [users] ([Email],[FirstName],[LastName],[Password],[PhoneNumber],[Img],[IsActive],[NotificationToken]) 
-    values (@Email, @FirstName, @LastName, @Password, @PhoneNumber, @Img, @IsActive, @NotificationToken)
-	set @UserID = @@identity 
-End 
-Go
+--create Proc Proc_Create_User
+--@Email nvarchar (150),
+--@FirstName nvarchar (150),
+--@LastName nvarchar (150),
+--@Password nvarchar(max),
+--@PhoneNumber nvarchar (150),
+--@Img nvarchar(max),
+--@IsActive bit,
+--@NotificationToken nvarchar(max),
+--@UserID int output
+--AS
+--IF NOT EXISTS (Select * From [users] Where Email=@Email or PhoneNumber=@PhoneNumber) 
+--Begin
+--    Insert [users] ([Email],[FirstName],[LastName],[Password],[PhoneNumber],[Img],[IsActive],[NotificationToken]) 
+--    values (@Email, @FirstName, @LastName, @Password, @PhoneNumber, @Img, @IsActive, @NotificationToken)
+--	set @UserID = @@identity 
+--End 
+--Go
 
---DECLARE @UserID int;
---exec Proc_Create_User 'test4@g.com','test4','test4', '12345', '123458',null,1,null, @UserID OUTPUT
---SELECT @UserID
+----DECLARE @UserID int;
+----exec Proc_Create_User 'test4@g.com','test4','test4', '12345', '123458',null,1,null, @UserID OUTPUT
+----SELECT @UserID
 
-Create proc Proc_Get_User_By_Id
-	@UserID int
-as
-	select  UserID,[Email],[FirstName],[LastName],[Password],[PhoneNumber],[Img],[IsActive],[NotificationToken]from users where UserID = @UserID
-go
+--Create proc Proc_Get_User_By_Id
+--	@UserID int
+--as
+--	select  UserID,[Email],[FirstName],[LastName],[Password],[PhoneNumber],[Img],[IsActive],[NotificationToken]from users where UserID = @UserID
+--go
 
---exec Proc_Get_User_By_Id 2
-
-
--------------------------------------------------List----------------------------------------------
-
-Create Proc Proc_Get_All_Lists_Created_By_User
-@CreatorID int
-AS
-Select * From [shopping_lists]
-where CreatorID = @CreatorID
-Go
+----exec Proc_Get_User_By_Id 2
 
 
-Create Proc Proc_Get_All_Lists_User_Is_A_Participant
-@UserID int
-AS
-Select * From [shopping_lists_users]
-where  [UserID]= @UserID
-Go
+---------------------------------------------------List----------------------------------------------
+
+
+
+
+--Create Proc Proc_Get_All_Lists_User_Is_A_Participant
+--@UserID int
+--AS
+--Select * From [shopping_lists_users]
+--where  [UserID]= @UserID
+--Go
 
 
